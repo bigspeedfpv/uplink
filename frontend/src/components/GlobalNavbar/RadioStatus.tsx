39aa6c7455dfc11289f76938a89b5b244d65af20
Andrew B <bigspeedfpv@gmail.com>
2023-01-26 23:46:15 -0500

    feat: add tooltip to connection

--- frontend/src/components/GlobalNavbar/RadioStatus.tsx
+++ frontend/src/components/GlobalNavbar/RadioStatus.tsx
@@ -1,29 +1,39 @@
-import React from "react";
-
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 
-import { Group, Text, ThemeIcon } from "@mantine/core";
+import { Group, Text, ThemeIcon, Tooltip } from "@mantine/core";
 import { IconDeviceGamepad2 } from "@tabler/icons";
 
 function RadioStatus() {
   const connected = useSelector((state: RootState) => state.connection.value);
 
   return (
-    <Group position="center" spacing="xs" py="xs">
-      <ThemeIcon
-        variant="outline"
-        sx={(theme) => ({
-          border: "none",
-          color: connected ? theme.colors.teal[4] : theme.colors.gray[6],
-        })}
-      >
-        <IconDeviceGamepad2 size={24} />
-      </ThemeIcon>
-      <Text color={connected ? "teal.4" : "gray.6"}>
-        {connected ? "Radio Connected" : "Radio Disconnected"}
-      </Text>
-    </Group>
+    <Tooltip
+      multiline
+      width={200}
+      withArrow
+      transitionDuration={300}
+      label={
+        connected
+          ? "Your radio is connected in DFU mode! You can now flash your radio."
+          : "To connect your radio, plug it in without powering it on."
+      }
+    >
+      <Group position="center" spacing="xs" py="xs">
+        <ThemeIcon
+          variant="outline"
+          sx={(theme) => ({
+            border: "none",
+            color: connected ? theme.colors.teal[4] : theme.colors.gray[6],
+          })}
+        >
+          <IconDeviceGamepad2 size={24} />
+        </ThemeIcon>
+        <Text color={connected ? "teal.4" : "gray.6"}>
+          {connected ? "Radio Connected" : "Radio Disconnected"}
+        </Text>
+      </Group>
+    </Tooltip>
   );
 }
 
